6629ccd2ed3e486d36f4e8d58ab3c88174929ecb
NoaGaray2023 <noa.garay.2118@lacetania.cat>
2023-02-14 13:33:34 +0100

    Carpetas i exercicis.

---
+++ Primers passos amb Arduino/001 Cicle de vida d'un programa/Exercici 4-Opcions/Exercici 4-Opcions.ino
@@ -0,0 +1,36 @@
+/*************************************************************
+**                         TÍTULO:                          **
+**                 Cicle de vida d'un programa              **
+**                                                          **
+**  NOM: Noa Garay Villarreal               DATA:13/02/2023 **
+*************************************************************/
+//************************ INCLUDE *************************** 
+
+
+//*********************** VARIABLES **************************
+
+
+
+//************************* SETUP ****************************
+
+void setup() 
+{               //configura el final de la salida
+Serial.begin(9600);
+ Serial.println("Escull numero de l'operacio que vols realitzar?");
+ Serial.println("1.Comprova numero de targeta de credit.");
+ Serial.println("2.Comprovar numero de compte bancari.");
+ Serial.println("3.Buscar un digit perdut de la targeta de credit.");
+ 
+}
+
+//************************* LOOP ******************************
+
+void loop() 
+{
+
+
+
+}
+
+//************************ FUNCIONS ***************************
+
