807b793b2c0abb254bdc1e3f15915c347bef7a10
NoaGaray2023 <noa.garay.2118@lacetania.cat>
2023-03-26 20:21:29 +0200

    Exercici 4-Array de 7 segments

--- Digital IO/003 Display de 7 segments/Exercici 4-Array de 7 segments/Exercici 4-Array de 7 segments.ino
+++ Digital IO/003 Display de 7 segments/Exercici 4-Array de 7 segments/Exercici 4-Array de 7 segments.ino
@@ -25,43 +25,43 @@ int segmentNum = 7;
 int pausa = 200;  
 const byte codis[] = {
 
-  B11111100,  // Representa el 0
-  B01100000,  // Representa el 1
-  B11011010,  // Representa el 2
-  B11110010,  // Representa el 3
-  B01100110,  // Representa el 4
-  B10110110,  // Representa el 5
-  B00111110,  // Representa el 6
-  B11100000,  // Representa el 7
-  B11111110,  // Representa el 8
-  B11100110,  // Representa el 9
-  B11101110,  // Representa la a (10)
-  B00111110,  // Representa la b (11)
-  B10011100,  // Representa la c (12)
-  B01111010,  // Representa la d (13)
-  B10011110,  // Representa la e (14)
-  B10001110,  // Representa la f (15)
-  B11110110,  // Representa la g (16)
-  B01101110,  // Representa la h (17)
-  B00001100,  // Representa la i (18)
-  B01111000,  // Representa la j (19)
-  B01101110,  // Representa la k (20)
-  B00011100,  // Representa la l (21)
-  B10101000,  // Representa la m (22)
-  B00101010,  // Representa la n (23)
-  B11111100,  // Representa la o (24)
-  B11001110,  // Representa la p (25)
-  B11100110,  // Representa la q (26)
-  B00001010,  // Representa la r (27)
-  B10110110,  // Representa la s (28)
-  B00111100,  // Representa la t (29)
-  B01111100,  // Representa la u (30)
-  B00111000,  // Representa la v (31)
-  B01010100,  // Representa la w (32)
-  B01101110,  // Representa la x (33)
-  B01110110,  // Representa la y (34)
-  B11011010,  // Representa la z (35)
-  B00000000,  // (36)
+  B11111100,  //0
+  B01100000,  //1
+  B11011010,  //2
+  B11110010,  //3
+  B01100110,  //4
+  B10110110,  //5
+  B00111110,  //6
+  B11100000,  //7
+  B11111110,  //8
+  B11100110,  //9
+  B11101110,  //a (10)
+  B00111110,  //b (11)
+  B10011100,  //c (12)
+  B01111010,  //d (13)
+  B10011110,  //e (14)
+  B10001110,  //f (15)
+  B11110110,  //g (16)
+  B01101110,  //h (17)
+  B00001100,  //i (18)
+  B01111000,  //j (19)
+  B01101110,  //k (20)
+  B00011100,  //l (21)
+  B10101000,  //m (22)
+  B00101010,  //n (23)
+  B11111100,  //o (24)
+  B11001110,  //p (25)
+  B11100110,  //q (26)
+  B00001010,  //r (27)
+  B10110110,  //s (28)
+  B00111100,  //t (29)
+  B01111100,  //u (30)
+  B00111000,  //v (31)
+  B01010100,  //w (32)
+  B01101110,  //x (33)
+  B01110110,  //y (34)
+  B11011010,  //z (35)
+  B00000000,  //Res(36)
   };
 //********** Setup ****************************************************************
 void setup()
